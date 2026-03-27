991eb5a7c709cc3752816b2883ce3c3a81356377
6UtkU8 <151883187+6UtkU8@users.noreply.github.com>
2026-03-27 19:35:57 +0300

    Delete SQLQuery2.sql

--- SQLQuery2.sql
+++
@@ -1,5 +0,0 @@
-INSERT INTO Users (FullName, Email, PhoneNumber, PasswordHash, Role, IsActive, CreatedAt)
-VALUES
-('System Admin', 'admin@kuryon.local', '5550000001', 'Admin123!', 'admin', 1, GETDATE()),
-('Ali Veli', 'courier@kuryon.local', '5550000002', 'Courier123!', 'courier', 1, GETDATE());
-SELECT * FROM Users;
